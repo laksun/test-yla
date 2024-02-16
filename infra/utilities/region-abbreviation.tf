b523deb9513241dd2b8fe893e70310463035f8dc
Yusuf.Aksun.ext <ylaksun@yahoo.com>
2024-02-16 16:53:30 +0000

    fix abbrev region

--- infra/utilities/region-abbreviation.tf
+++ infra/utilities/region-abbreviation.tf
@@ -3,11 +3,18 @@ locals {
     "us-east-1",
     "us-west-1",
     "eu-central-1",
+    "ap-southeast-1",
     // Add more regions as needed
   ]
 
   region_abbreviations = { for region in local.aws_regions :
-    region => join("", [substr(region, 0, 2), substr(region, 3, 1), substr(region, 7, 1)])
+    region => join("",
+      [
+        substr(region, 0, 2), # First 2 letters of the region part.
+        substr(region, 3, 1), # First letter of the geographical descriptor.
+        substr(region, -1, 1) # Last character, assuming it's the numerical identifier.
+      ]
+    )
   }
 }
 
